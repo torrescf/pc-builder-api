1dba6787c018232ec353dc7cbd6af5e8ced44bf4
torrescf <joaopedrooliveiradejesusm@gmail.com>
2026-09-23 22:02:31 -0300

    fix: improve budget authorization and validation

--- src/routes/budget.routes.ts
+++ src/routes/budget.routes.ts
@@ -8,13 +8,21 @@ import {
   deleteBudgetController,
   updateBudgetStatusController,
 } from '../controllers/budget.controller';
+import { roleMiddleware } from '../middlewares/auth';
 
 const router = Router();
 
 router.get('/', getBudgets);
 router.get('/:id', getBudget);
+
 router.post('/', createBudgetController);
-router.patch('/id/status', updateBudgetStatusController);
+
+router.patch(
+  '/:id/status',
+  roleMiddleware(['ADMIN']),
+  updateBudgetStatusController
+);
+
 router.patch('/:id', updateBudgetController);
 router.delete('/:id', deleteBudgetController);
 
